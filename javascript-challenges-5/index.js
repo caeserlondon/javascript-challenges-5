22951d83042b21e5ed7540581863f1fc91c97b86
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-04-14 19:40:01 +0700

    challenge 1 solutin 2

--- javascript-challenges-5/index.js
+++ javascript-challenges-5/index.js
@@ -1,15 +1,25 @@
 /////// CHALLENGE 1  UNIQUE VALUES
 ///  find unique values by using Set() data structure
+/// solutin 1
+// function unique(str) {
+// 	let tempStr = new Set();
+// 	for (let letter of str) {
+// 		if (tempStr.has(letter)) {
+// 			return false;
+// 		}
+// 		tempStr.add(letter);
+// 	}
+// 	return true;
+// }
 
+// console.log(unique("abcde"));
+// //true
+// console.log(unique("abacdefb"));
+// //false
+//////////
+/// solution 2
 function unique(str) {
-	let tempStr = new Set();
-	for (let letter of str) {
-		if (tempStr.has(letter)) {
-			return false;
-		}
-		tempStr.add(letter);
-	}
-	return true;
+	return new Set(str).size === str.length;
 }
 
 console.log(unique("abcde"));
