069488961035f98c512f4f6cfe03f4599c1ebd75
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-04-15 12:17:55 +0700

    challenge 6

--- javascript-challenges-5/index.js
+++ javascript-challenges-5/index.js
@@ -146,29 +146,47 @@
 /// if two numbers are repeated then repeat the first
 /// if none repeated then return -1
 
-function testRepeat(arr) {
-	let max = 1;
-	let position = 0;
-	let value = -1;
-	let tempNumbers = arr.reduce((acc, curr, index) => {
-		acc[curr] = acc[curr]
-			? { ...acc[curr], amount: acc[curr]["amount"] + 1 }
-			: { amount: 1, index };
-		let amount = acc[curr].amount;
-		let place = acc[curr].index;
-		if (amount > max || (amount === max && place <= position && amount > 1)) {
-			max = amount;
-			value = curr;
-			position = place;
-		}
-		return acc;
-	}, {});
-	return value;
+// function testRepeat(arr) {
+// 	let max = 1;
+// 	let position = 0;
+// 	let value = -1;
+// 	let tempNumbers = arr.reduce((acc, curr, index) => {
+// 		acc[curr] = acc[curr]
+// 			? { ...acc[curr], amount: acc[curr]["amount"] + 1 }
+// 			: { amount: 1, index };
+// 		let amount = acc[curr].amount;
+// 		let place = acc[curr].index;
+// 		if (amount > max || (amount === max && place <= position && amount > 1)) {
+// 			max = amount;
+// 			value = curr;
+// 			position = place;
+// 		}
+// 		return acc;
+// 	}, {});
+// 	return value;
+// }
+
+// console.log(testRepeat([5, 2, 2, 1, 5]));
+// // return 5
+// console.log(testRepeat([6, 5, 5, 10, 10, 10]));
+// // return 10
+// console.log(testRepeat([3, 4, 1, 6, 10]));
+// // return -1;
+
+/////////////////////////////////////////////////////////////
+
+///// CHALLENGE 6
+//prime number = can only be divided by itself or one.
+
+function isPrime(num) {
+	return num;
 }
 
-console.log(testRepeat([5, 2, 2, 1, 5]));
-// return 5
-console.log(testRepeat([6, 5, 5, 10, 10, 10]));
-// return 10
-console.log(testRepeat([3, 4, 1, 6, 10]));
-// return -1;
+console.log(isPrime(8));
+// false
+console.log(isPrime(11));
+// true
+console.log(isPrime(121));
+// false
+console.log(isPrime(127));
+// true
