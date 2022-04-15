ead5d988784e8ac53f2d9e4c1730e79f3ac3b438
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-04-15 10:00:03 +0700

    challenge 3 solution

--- javascript-challenges-5/index.js
+++ javascript-challenges-5/index.js
@@ -31,48 +31,83 @@
 //// CHALLENGE 2
 /// find unique object property values
 
-let products = [
-	{
-		title: "Iphone 8",
-		company: "apple",
-	},
-	{
-		title: "Galaxy",
-		company: "samsong",
-	},
-	{
-		title: "Iphone 7",
-		company: "apple",
-	},
-	{
-		title: "Iphone Xs",
-		company: "apple",
-	},
-	{
-		title: "HTC phone",
-		company: "htc",
-	},
-	{
-		title: "Galaxy HD",
-		company: "samsong",
-	},
-];
+// let products = [
+// 	{
+// 		title: "Iphone 8",
+// 		company: "apple",
+// 	},
+// 	{
+// 		title: "Galaxy",
+// 		company: "samsong",
+// 	},
+// 	{
+// 		title: "Iphone 7",
+// 		company: "apple",
+// 	},
+// 	{
+// 		title: "Iphone Xs",
+// 		company: "apple",
+// 	},
+// 	{
+// 		title: "HTC phone",
+// 		company: "htc",
+// 	},
+// 	{
+// 		title: "Galaxy HD",
+// 		company: "samsong",
+// 	},
+// ];
 
-/// solution 1
+// /// solution 1
 
+// // function getUnique(arr) {
+// // 	let tempArr = arr.map((item) => item.company);
+// // 	return [...new Set(tempArr)];
+// // }
+// // console.log(getUnique(products));
+
+// /// solution 2
 // function getUnique(arr) {
-// 	let tempArr = arr.map((item) => item.company);
-// 	return [...new Set(tempArr)];
+// 	return [
+// 		...arr.reduce((acc, curr) => {
+// 			acc.add(curr.company);
+// 			return acc;
+// 		}, new Set()),
+// 	];
 // }
 // console.log(getUnique(products));
 
-/// solution 2
-function getUnique(arr) {
-	return [
-		...arr.reduce((acc, curr) => {
-			acc.add(curr.company);
-			return acc;
-		}, new Set()),
-	];
+////////////////
+/// CHALLENGE 3
+//return first word with the greatest number of repeated letters
+
+function countLetters(str) {
+	let tempArr = str.split(" ");
+	tempArr = tempArr.map((item) => {
+		let tempItemArr = item.toLowerCase().split("");
+		return tempItemArr.reduce(
+			(acc, curr) => {
+				acc[curr] = acc[curr] ? acc[curr] + 1 : 1;
+				if (acc[curr] > acc.max) {
+					acc.max = acc[curr];
+				}
+				return acc;
+			},
+			{ max: 1, word: item }
+		);
+	});
+	let amount = 1;
+	let word = "";
+	for (let item of tempArr) {
+		if (item.max > amount) {
+			amount = item.max;
+			word = item.word;
+		}
+	}
+	if (amount > 1) {
+		return word;
+	}
+
+	return -1;
 }
-console.log(getUnique(products));
+console.log(countLetters("Javascript is the greatest programming languageee"));
